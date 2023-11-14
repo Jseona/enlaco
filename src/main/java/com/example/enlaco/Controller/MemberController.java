1abc151ea99074255aecf58016dc49e21af3cb53
adubnos <gp39tu8526@gmail.com>
2023-11-14 15:44:18 +0900

    2023.11.14 서동관

--- src/main/java/com/example/enlaco/Controller/MemberController.java
+++ src/main/java/com/example/enlaco/Controller/MemberController.java
@@ -1,4 +1,25 @@
 package com.example.enlaco.Controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/member")
 public class MemberController {
+    @GetMapping("/insert")
+    public String insertForm() throws Exception {
+        return "/member/insert";
+    }
+    @PostMapping("/insert")
+    public String insertProc() throws Exception {
+        return "redirect:/";
+    }
+    @GetMapping("/login")
+    public String login() throws Exception {
+        return "/member/login";
+    }
 }
--- src/main/java/com/example/enlaco/Controller/RecipeController.java
+++ src/main/java/com/example/enlaco/Controller/RecipeController.java
@@ -1,4 +1,37 @@
 package com.example.enlaco.Controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/recipe")
 public class RecipeController {
+    @GetMapping("/detail")
+    public String detail() throws Exception {
+        return "/recipe/detail";
+    }
+    @GetMapping("insert")
+    public String insertForm() throws Exception {
+        return "/recipe/insert";
+    }
+    @PostMapping("insert")
+    public String insertProc() throws Exception {
+        return "redirect:/recipe/list";
+    }
+    @GetMapping("list")
+    public String list() throws Exception {
+        return "/recipe/list";
+    }
+    @GetMapping("modify")
+    public String modifyForm() throws Exception {
+        return "/recipe/modify";
+    }
+    @PostMapping("modify")
+    public String modifyProc() throws Exception {
+        return "redirect:/member/mypage";
+    }
 }
