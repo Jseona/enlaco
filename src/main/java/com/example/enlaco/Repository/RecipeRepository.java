ea1dcc3b99254a383b1c42f2e617f0efc3fe14e4
woori <kihee4501@gmail.com>
2023-11-16 10:25:42 +0900

    2023-11-16 정선아 10:25

--- src/main/java/com/example/enlaco/Repository/RecipeRepository.java
+++ src/main/java/com/example/enlaco/Repository/RecipeRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Integer> {
     @Modifying
-    @Query(value = "UPDATE RecipeEntity u SET  u.rviewcnt=u.rviewcnt+1 WHERE u.rid=:rid",
+    @Query(value = "UPDATE recipe u SET  u.rviewcnt=u.rviewcnt+1 WHERE u.rid=:rid",
     nativeQuery = true)
     public void rviewcnt(@Param("rid") Integer rid);
 
