410b5f11f0caa6ae6b659faf85a809605ece4f71
woori <kihee4501@gmail.com>
2023-11-21 15:01:18 +0900

    2023-11-21 정선아 14:55

--- src/main/java/com/example/enlaco/Controller/MemberController.java
+++ src/main/java/com/example/enlaco/Controller/MemberController.java
@@ -12,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 
