7506e587f3b9227e533fe735802f20a8fd9c6801
Jseona <jsa102942@gmail.com>
2023-11-27 09:43:14 +0900

    2023-11-27 정선아 09:42

---
+++ src/main/java/com/example/enlaco/Controller/FlaskController.java
@@ -0,0 +1,39 @@
+package com.example.enlaco.Controller;
+
+import com.example.enlaco.DTO.FlaskDTO;
+import com.example.enlaco.Util.Flask;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FlaskController {
+    //S3 이미지 정보
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;
+    @Value("${cloud.aws.region.static}")
+    public String region;
+    @Value("${imgUploadLocation}")
+    public String folder;
+
+    @Autowired
+    private Flask flask;
+
+    @GetMapping("/storage/image")
+    public String image() throws Exception {
+        return "storage/image";
+    }
+
+    @PostMapping("/storage/result")
+    public String result(@RequestParam("file") MultipartFile file, Model model) throws Exception {
+        FlaskDTO dto = flask.requestToFlask(file);
+
+        model.addAttribute("dto", dto);
+        return "storage/result";
+    }
+}
