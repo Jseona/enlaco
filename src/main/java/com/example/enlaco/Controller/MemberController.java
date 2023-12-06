bc309eaf115754d87ff54df095ba18f31e25a2bb
Jseona <jsa102942@gmail.com>
2023-12-06 12:04:02 +0900

    2023-12-06 정선아 12:03

--- src/main/java/com/example/enlaco/Controller/MemberController.java
+++ src/main/java/com/example/enlaco/Controller/MemberController.java
@@ -7,6 +7,7 @@ import com.example.enlaco.Service.RecipeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
@@ -52,9 +54,10 @@ public class MemberController {
 
         try {
             memberService.saveMember(memberDTO);
-            redirectAttributes.addAttribute("errorMessage", "가입을 축하합니다.");
         } catch (IllegalStateException e) {
             redirectAttributes.addAttribute("errorMessage", e.getMessage());
+            return "member/insert";
+        } catch (DataIntegrityViolationException e) {
             return "member/insert";
         }
         return "redirect:/";
